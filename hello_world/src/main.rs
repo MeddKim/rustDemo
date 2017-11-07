27121dcf2ca9c436899600aadc2b058340216aa4
wang.zhongyi <meddkim@163.com>
2017-11-07 09:39:49 +0800

    v0.0.1

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-	println!("hello,world"); 	  
+    println!("Guess the number");
+
+    println!("Please input you guess.");
+
+    // let创建变量绑定（默认不可变），mut让绑定可变
+    let mut guess = String::new();
+
+    //std::io::stdin()返回一个指向终端标准输入的句柄
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed :{}", guess);
 }
