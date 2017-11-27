1ccf2952e8da4af3d37887616dc358499688c0d1
wang.zhongyi <meddkim@163.com>
2017-11-27 18:24:08 +0800

    add demo

---
+++ README.md
@@ -0,0 +1,13 @@
+rust学习
+
+## Cargo使用
+编译
+```
+cargo build
+```
+运行
+```
+cargo run
+```
+
+
---
+++ varDemo/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+	let a:i32 = 23;
+	let b:i16 = -16;
+
+//	let c:u16 = -18;
+
+	println!("{}",a);
+	println!("{}",b);
+}
