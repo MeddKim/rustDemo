3270505e569f96d51432308f807ff6d53b4c9552
unknown <meddkim@163.com>
2018-05-24 19:01:11 +0800

    fix

---
+++ practice/chapter01/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+//==========================
+//    let rust = "rust";
+//    println!("Hello, {}!",rust);
+//==========================
+//    let a1 = 5;
+//    let a2:i32 = 5;
+//    assert_eq!(a1,a2);
+//
+//    let b1:u32 = 5;
+//    assert_eq!(a1,b1)
+//==============================
+//    let (a, mut b):(bool,bool) = (true,false);
+//    println!("a = {:?}, b = {:?}",a,b);
+//    b = true;
+//    assert_eq!(a,b);
+//===============================
+    let str = "hello,world";
+    println!("{}",str);
+    str = "hello rust";
+    println!("{}",str)
+}
